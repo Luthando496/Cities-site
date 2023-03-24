4933556d68d316c3ed62b995beb17a861dd0655b
unknown <luthandodidiza@gmail.com>
2023-03-24 11:48:33 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="">
+      <h3 className="text-5xl text-sky-500 text-center">HEllo Out There</h3>
+    </div>
+  )
+}
+
+export default App
